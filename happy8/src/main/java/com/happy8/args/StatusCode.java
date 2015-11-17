232d4b19e6e4a54b7de65b70aec64c613c3f86f8
tianxiaowei <tianxiaowei@feinno.com>
2015-11-17 13:27:51 +0800

    add status Code

---
+++ happy8/src/main/java/com/happy8/args/StatusCode.java
@@ -0,0 +1,14 @@
+package com.happy8.args;
+
+public class StatusCode {
+	private int statusCode;
+
+	public int getStatusCode() {
+		return statusCode;
+	}
+
+	public void setStatusCode(int statusCode) {
+		this.statusCode = statusCode;
+	}
+	
+}
