b0c467ce042a1927721f9a3b391289ad8e308e9a
tianxiaowei <tianxiaowei@feinno.com>
2015-12-04 17:39:58 +0800

    add det

---
+++ happy8/src/main/java/com/happy8/args/OrderDetailItem.java
@@ -0,0 +1,32 @@
+package com.happy8.args;
+
+public class OrderDetailItem {
+	private String userId;
+	private int tableId;
+	private String date;
+	private int gameTime;
+	public String getUserId() {
+		return userId;
+	}
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+	public int getTableId() {
+		return tableId;
+	}
+	public void setTableId(int tableId) {
+		this.tableId = tableId;
+	}
+	public String getDate() {
+		return date;
+	}
+	public void setDate(String date) {
+		this.date = date;
+	}
+	public int getGameTime() {
+		return gameTime;
+	}
+	public void setGameTime(int gameTime) {
+		this.gameTime = gameTime;
+	}
+}
