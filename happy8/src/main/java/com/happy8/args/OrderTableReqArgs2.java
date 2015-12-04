33433dce9bc9f0a4578b1adb5c550dafe439407f
tianxiaowei <tianxiaowei@feinno.com>
2015-12-04 17:39:30 +0800

    add arg2

---
+++ happy8/src/main/java/com/happy8/args/OrderTableReqArgs2.java
@@ -0,0 +1,19 @@
+package com.happy8.args;
+
+public class OrderTableReqArgs2 {
+	private String detail;
+	private double amount;
+	public String getDetail() {
+		return detail;
+	}
+	public void setDetail(String detail) {
+		this.detail = detail;
+	}
+	public double getAmount() {
+		return amount;
+	}
+	public void setAmount(double amount) {
+		this.amount = amount;
+	}
+	
+}
